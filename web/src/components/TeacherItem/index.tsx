ac558cc22e80714b64063e7e70ba23bc327facb6
Igor Sasaki <igor-sasaki@hotmail.com>
2020-08-03 23:15:46 -0300

    Primeiro dia da Next level Week promovido pela Rocketseat

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css';
+
+function TeacherItem() {
+    return(
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars0.githubusercontent.com/u/23747927?s=400&u=8b23092773d60fbe9945d6605fe2e1450b9f79d3&v=4" alt="Igor Sasaki"/>
+                <div>
+                    <strong>Igor Sasaki</strong>
+                    <span>Desenvolvedor FullStack</span>
+                </div>
+            </header>
+            <p>
+                Entusiasta das melhores tecnologias de programação web avançada
+                <br/><br/>
+                Apaixonado por fazer sistemas web utilizando a omnistack
+            </p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 120,00</strong>
+                </p>
+                <button>
+                    <img src={whatsappIcon} alt="Whats"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
